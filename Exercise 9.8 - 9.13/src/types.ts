982746c795ca1ffbbc8212670c36ea2db2dd58e8
JahdielArciniegas <arciniegasjahdiel@gmail.com>
2024-09-29 17:33:57 -0400

    Exercise 9.22

--- Exercise 9.8 - 9.13/src/types.ts
+++ Exercise 9.8 - 9.13/src/types.ts
@@ -4,9 +4,51 @@ export interface Diagnoses {
   latin? : string
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Entry{
+
+interface SickLeave {
+  startDate : string,
+  endDate : string
 }
+
+interface Discharge {
+  date : string,
+  criteria: string
+}
+
+interface BaseEntry {
+  id:string,
+  description: string,
+  date: string,
+  specialist : string,
+  diagnosisCode?: Array<Diagnoses['code']>
+}
+
+interface OccupationalHealthCare extends BaseEntry {
+  type : 'occupationalHealthCare',
+  sickLeave : SickLeave
+}
+
+interface Hospital extends BaseEntry{
+  type : 'Hospital',
+  discharge : Discharge
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type : "HealhCheck",
+  healthCheckRating : HealthCheckEntry
+}
+
+export type Entry = 
+| Hospital
+| OccupationalHealthCare
+| HealthCheckEntry;
 
 export enum Gender{
   Male = "male",
@@ -24,5 +66,7 @@ export interface Patients {
   entries: Entry[]
 }
 
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T,K> : never;
+export type EntryWithoutId = UnionOmit<Entry, 'id'>;
 export type getPatientsNonSsn = Omit<Patients, 'ssn' | 'entries'>;
 export type newPatients = Omit<Patients, 'id' >;
